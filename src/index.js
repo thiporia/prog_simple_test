44b3e4d9d725fcc62736eaea5e78cea2f3d267ae
thiporia <ssoju2827@gmail.com>
2020-08-26 21:52:33 +0900

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.jpg';
+import Data from './data.xml';
+import DataJson from './data.json';
 
 function component() {
 	const element = document.createElement('div');
@@ -6,6 +10,16 @@ function component() {
 	// Lodash, currently included via a script, is required for this line to work
 	// Lodash, now imported by this script
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+	console.log(Data);
+	console.log(DataJson);
 
 	return element;
 }
