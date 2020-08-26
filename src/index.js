fde35360bcad6f5534953e13ec14ec55e321dde6
thiporia <ssoju2827@gmail.com>
2020-08-26 21:59:45 +0900

    asset management tear down

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';
-import Data from './data.xml';
-import DataJson from './data.json';
 
 function component() {
 	const element = document.createElement('div');
 
-	// Lodash, currently included via a script, is required for this line to work
-	// Lodash, now imported by this script
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-	console.log(Data);
-	console.log(DataJson);
 
 	return element;
 }
