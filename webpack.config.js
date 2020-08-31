1e367f1c73c040ad5da1a72c46e5984ab976087e
thiporia <ssoju2827@gmail.com>
2020-08-31 22:02:13 +0900

    feat(development): 웹팩 org의 development

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+	mode: 'development',
   entry: {
     app: './src/index.js',
 		print: './src/print.js',
   },
+	devtool: 'inline-source-map',
 	plugins: [
-	  new CleanWebpackPlugin(),
+	  new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     new HtmlWebpackPlugin({
       title: 'Output Management',
     }),
@@ -16,6 +18,7 @@ module.exports = {
 	output: {
 	  filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/',
 	}
 };
 
